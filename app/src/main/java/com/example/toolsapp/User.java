5872941f63b069d9d25db6146e064734cc6c9168
Maharshi Trivedi <maharshitrivedi8686@gmail.com>
2019-10-21 14:18:50 -0400

    I have put the conditions in the sign in page.

--- app/src/main/java/com/example/toolsapp/User.java
+++ app/src/main/java/com/example/toolsapp/User.java
@@ -2,32 +2,49 @@ package com.example.toolsapp;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+public class User implements Serializable
+{
     private String name;
     private String username;
     private String email;
     private String password;
 
-    public User(String name, String username, String email, String password) {
+    public User(String name, String username, String email, String password)
+    {
         this.name = name;
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public String getUsername() {
-        return username;
+    public void setName(String name)
+    {
+        this.name = name;
     }
 
-    public void setUsername(String username) {
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+
+    public void setUsername(String username)
+    {
         this.username = username;
     }
 
-    public String getEmail() {
+    public String getEmail()
+    {
         return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
     }
 }
